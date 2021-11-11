c954408c50007f355c9c53e0e6056c8aaffe97bb
Felipe Arias <felipe2arias@hotmail.com>
2021-11-11 12:07:41 -0300

    mapeo de entidades terminado

---
+++ spotify/src/main/java/com/bootcamp/spotify/service/ArtistService.java
@@ -0,0 +1,22 @@
+package com.bootcamp.spotify.service;
+
+import com.bootcamp.spotify.controller.request.AlbumRequest;
+import com.bootcamp.spotify.controller.request.ArtistRequest;
+import com.bootcamp.spotify.domain.model.Album;
+import com.bootcamp.spotify.domain.model.Artist;
+
+import java.util.List;
+
+public interface ArtistService {
+    Artist getArtist(Long idArtist);
+
+    List<Artist> getArtist();
+
+    Artist createArtist(ArtistRequest request);
+
+    Artist editArtist(ArtistRequest request, Long idArtist);
+
+    Artist deleteArtist(Long idArtist);
+
+}
+
